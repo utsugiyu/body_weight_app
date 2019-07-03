242b5164e49722157e822164b77ecccec059601d
Yu Utsugi <utsugiu@gmail.com>
2019-07-03 17:44:26 +0900

    add layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -21,4 +21,53 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?][data-method=?]", logout_path, "delete"
   end
+
+  test "period tabs test" do
+    log_in_as(@user)
+    get user_path(@user)
+    assert_select 'a.active', "3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=week"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=month"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=all"
+    get "/users/#{@user.id}?duration=week"
+    assert_select 'a.active', "Week"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=week"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=month"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=all"
+    get "/users/#{@user.id}?duration=month"
+    assert_select 'a.active', "Month"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=week"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=month"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=all"
+    get "/users/#{@user.id}?duration=all"
+    assert_select 'a.active', "All"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=week"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=month"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=all"
+    get "/users/#{@user.id}?duration=3days"
+    assert_select 'a.active', "3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=3days"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=week"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=month"
+    assert_select "a[href=?]", "/users/#{@user.id}?duration=all"
+  end
+
+  test "delete button test" do
+    log_in_as(@user)
+    get user_path(@user)
+    first_records = @user.records.take(7)
+    first_records.each do |record|
+      assert_select "a[href=?][data-method=?]", "/records/#{record.id}", "delete"
+    end
+  end
+
+  test "paginate test" do
+    log_in_as(@user)
+    get user_path(@user)
+    assert_select "a[href=?]", "/users/#{@user.id}?page=2"
+  end
 end
