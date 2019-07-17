e53f9b3e228b4ea469a099cd75d83e05a3263c42
Yu Utsugi <utsugiu@gmail.com>
2019-07-17 16:39:40 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -88,13 +88,16 @@ class UsersController < ApplicationController
     :grant_type => "authorization_code"}
     )
 
+    access_token = token[:access_token]
+    refresh_token = token[:refresh_token]
+
     secret = SecureRandom.random_bytes(32)
     encryptor = ::ActiveSupport::MessageEncryptor.new(secret, cipher: 'aes-256-cbc')
-    encrypt_access_token = encryptor.encrypt_and_sign(token)
+    encrypt_access_token = encryptor.encrypt_and_sign(access_token)
+    encrypt_refresh_token = encryptor.encrypt_and_sign(refresh_token)
+    current_user.update_attributes(access_token: encrypt_access_token, refresh_token: encrypt_refresh_token)
 
-    current_user.update_attributes(access_token: encrypt_access_token)
-
-    redirect_to "/users/#{current_user.id}?token=#{encrypt_access_token}"
+    redirect_to "/users/#{current_user.id}?token=#{token}&refresh=#{token.refresh_token}"
   end
 
   private
